d657ce6816b77dd0de9f787b45ff748ea1bea7cd
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2023-12-14 20:24:38 -0300

    Commit peliculas

--- lib/Screens/Pelicula_recomendada.dart
+++ lib/Screens/Pelicula_recomendada.dart
@@ -1,8 +1,34 @@
 import 'package:app_laboflutter/Widget/Drawer_menu.dart';
+import 'package:app_laboflutter/Widget/card_image.dart';
+import 'package:app_laboflutter/Widget/card_image2.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class PeliculaRecomendada extends StatelessWidget{
-  const PeliculaRecomendada({super.key});
+class PeliculaRecomendada extends StatefulWidget{
+  PeliculaRecomendada({super.key});
+
+  @override
+  State<PeliculaRecomendada> createState() => _PeliculaRecomendadaState();
+}
+
+class _PeliculaRecomendadaState extends State<PeliculaRecomendada> {
+  final List peliculas=[
+    {"titulo":"Flash","url":"assets/images/Flash.jpg"},
+    {"titulo":"SawX","url":"assets/images/Saw.jpg"},
+    {"titulo":"Scary Movie 4","url":"assets/images/Scary movie.jpg"},
+    {"titulo":"Blue Bettle","url":"assets/images/Blue Bettle.jpg"},
+    {"titulo":"John Wick 4","url":"assets/images/John Wick 4.jpg"},
+    {"titulo":"Leo","url":"assets/images/Leo.jpg"},
+    {"titulo":"Five Nights at Freddy: La pelicula","url":"assets/images/Five Nights.jpg"},
+    {"titulo":"Los juegos del hambre la balada de pajaros cantores y serpientes","url":"assets/images/Los juegos del hambre.jpg"},
+    {"titulo":"Barbie","url":"assets/images/Barbie.jpg"},
+    {"titulo":"Avatar el camino del agua","url":"assets/images/avatar.jpg"}
+  ];
+  List peliculas_puntuadas=[
+  ];
+
+  var indice=0;
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -15,7 +41,10 @@ class PeliculaRecomendada extends StatelessWidget{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("hola")
+            if(indice<peliculas.length)
+                CardImage2(url:peliculas[indice]["url"])
+            else
+              Text("no hay mas peliculas")
             ]
             ),
       ),
@@ -34,24 +63,83 @@ class PeliculaRecomendada extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             FloatingActionButton(
-            heroTag: "fab_dislike", 
+            heroTag: "fab_history", 
           onPressed:(){
+            var largo_fav=peliculas_puntuadas.length;
+            var largo=peliculas.length;
+            showDialog(context: context, builder: (BuildContext context){
+              return AlertDialog(
+                title: Text("Peliculas que puntuaste"),
+                content:Column(
+                  children: [
+                    for(var pelicula in peliculas_puntuadas)
+                Column(
+                  children: [
+                    Text("Título: ${pelicula['titulo']}"),
+                    Text("Puntuación: ${pelicula['puntuacion'].toString()}"),
+                  ],
+                )
+                ]),
+                actions: [
+                  TextButton(onPressed:(){ 
+                    Navigator.of(context).pop();},
+                    child: Text("aceptar"),)
+                ],
+              );
+            });
           },
           backgroundColor: Color.fromARGB(255, 170, 6, 6),
-          child: const Icon(Icons.thumb_down_rounded),
+          child: const Icon(Icons.history),
           ),
           FloatingActionButton(
-            heroTag: "fab_like",           
+            heroTag: "fab_puntuar",           
             onPressed:(){
-             print ("hola");
+            showDialog(context: context, builder: (BuildContext context){
+              double Puntuacion=0;
+              return AlertDialog(
+                title: Text("Pelicula para puntuar"),
+          content: Column(
+            children: [
+              Text("Puntua la pelicula"),
+              RatingBar.builder(
+                initialRating: 3,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                itemBuilder: (context, _) => Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                ),
+                onRatingUpdate: (rating) {
+                  Puntuacion=rating;
+                },
+              ),
+            ],
+          ),
+                actions: [
+                  TextButton(onPressed:(){ 
+                    setState(() {
+                          peliculas_puntuadas.add({
+                            "titulo":peliculas[indice]["titulo"],
+                            "puntuacion": Puntuacion});
+                          indice=indice+1;
+                      });
+                    Navigator.of(context).pop();},
+                    child: Text("aceptar"),)
+                ],
+              );
+            });
           },
           backgroundColor: Color.fromARGB(255, 39, 123, 201),
-          child: const Icon(Icons.thumb_up_alt_rounded),
+          child: const Icon(Icons.star),
           ),
           FloatingActionButton(
             heroTag: "fab_next",          
-            onPressed:(){
-          print("Next");
+            onPressed:(){setState(() {
+              indice=indice+1;
+            });
           },
           child: const Icon(Icons.arrow_forward_outlined),
           )
