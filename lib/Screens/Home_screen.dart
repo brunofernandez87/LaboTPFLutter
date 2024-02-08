8034171fc67a2cc7d94c3d0b4acfc99ca547e335
Sofii2301 <sofialvarezrodero97@gmail.com>
2024-02-08 04:20:56 -0300

    comentado

--- lib/Screens/Home_screen.dart
+++ lib/Screens/Home_screen.dart
@@ -14,7 +14,78 @@ class _HomeScreenState extends State<HomeScreen> {
   //ActorProvider actorProvider = ActorProvider();
   @override
   Widget build(BuildContext context) {
-    const List peliculasyTVseries = [];
+    const List peliculasyTVseries = [
+      {
+        'url': 'assets/images_Peliculas/Avatar.jpg',
+        'name': 'Avatar: El camino del agua',
+        'duration': 'Duracion: 2h 39m'
+      },
+      {
+        'url': 'assets/images_Peliculas/Five Nights.jpg',
+        'name': 'Five Nights at Freddy: La pelicula',
+        'duration': 'Duracion: 1h 50m'
+      },
+      {
+        'url': 'assets/images_Peliculas/Flash.jpg',
+        'name': 'Flash',
+        'duration': 'Duracion: 1h 30m'
+      },
+      {
+        'url': 'assets/images_Peliculas/Leo.jpg',
+        'name': 'Leo',
+        'duration': 'Duracion: 1h 42m'
+      },
+      {
+        'url': 'assets/images_Peliculas/Saw.jpg',
+        'name': 'Saw X',
+        'duration': 'Duracion: 1h'
+      },
+      {
+        'url': 'assets/images_Peliculas/Spiderman.jpg',
+        'name': 'Spiderman: No way home',
+        'duration': 'Duracion: 1h 30m'
+      },
+      {
+        'url': 'assets/images_Peliculas/Wonka.jpg',
+        'name': 'Wonka',
+        'duration': 'Duracion: 1h 57m'
+      },
+      {
+        'url': 'assets/images_TvSeries/Black Mirror.jpg',
+        'name': 'Black Mirror',
+        'duration': 'Duracion: 30 episodios'
+      },
+      {
+        'url': 'assets/images_TvSeries/Narcos.jpg',
+        'name': 'Narcos',
+        'duration': 'Duracion: 45 episodios'
+      },
+      {
+        'url': 'assets/images_TvSeries/Peaky Blinders.jpg',
+        'name': 'Peaky Blinders',
+        'duration': 'Duracion: 50 episodios'
+      },
+      {
+        'url': 'assets/images_TvSeries/Sherlock.jpg',
+        'name': 'Sherlock',
+        'duration': 'Duracion: 27 episodios'
+      },
+      {
+        'url': 'assets/images_TvSeries/Stranger Things.jpg',
+        'name': 'Stranger Things',
+        'duration': 'Duracion: 45 episodios'
+      },
+      {
+        'url': 'assets/images_TvSeries/Breaking Bad.jpg',
+        'name': 'Breaking Bad',
+        'duration': 'Duracion: 65 episodios'
+      },
+      {
+        'url': 'assets/images_TvSeries/Ozark.jpg',
+        'name': 'Ozark',
+        'duration': 'Duracion: 52 episodios'
+      }
+    ];
     List actores = [];
 
     //actores = actorProvider.obtenerListaActores();
